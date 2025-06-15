08162cf3a06e22667aaaaaf64722735bd2cd21ea
zezor <kemma2993@gmail.com>
2025-06-14 17:17:49 -0800

    first commit to virtual

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO Customers (CUSTOMER_NAME, EMAIL, ADDRESS)
-VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1,'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-INSERT INTO Customers (CUSTOMER_ID,CUSTOMER_NAME, EMAIL, ADDRESS)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2,'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
