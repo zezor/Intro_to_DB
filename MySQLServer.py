a7f8e19911b056d2cad71192c8f48d01cd29fa94
zezor <kemma2993@gmail.com>
2025-06-14 16:29:28 -0800

    first commit to virtual

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,8 +16,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+    except mysql.connector.Error:
+        print(f"Error while connecting to MySQL: {Error}")
 
     finally:
         if 'cursor' in locals() and cursor:
